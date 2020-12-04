e4ec65096cfb3197eff276f83d51381a2a256463
Juveria Manzar <juveriamanzar29@gmail.com>
2020-12-04 10:02:11 +0530

    add-category-post

--- CRUD_APP/Controllers/CategoryController.cs
+++ CRUD_APP/Controllers/CategoryController.cs
@@ -25,5 +25,15 @@ namespace CRUD_APP.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
